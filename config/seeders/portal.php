8b012e3c50e8a1a61a42b73e9a677c016e195983
stijn1290 <153715940+stijn1290@users.noreply.github.com>
2026-03-26 19:18:34 +0100

    Added seeder for roles and portals and users

---
+++ config/seeders/portal.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'name' => 'portalease',
+    'email' => 'portalease@portalease.com',
+    'branding_color' => '#33C1FF',
+    'logo' => 'portalEaseLogo.png'
+];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Portal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call(RoleSeeder::class);
+        $portal = Portal::create([
+            'name' => config('seeders.portal.name'),
+            'email' => config('seeders.portal.email'),
+            'branding_color' => config('seeders.portal.branding_color'),
         ]);
+        $admin = User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@portalease.com',
+            'portal_id' => $portal->id,
+        ]);
+        $admin->assignRole('service_provider');
+        $admin->assignRole('admin');
+
+
+        $client = User::factory()->create([
+            'name' => 'client',
+            'email' => 'client@portalease.com',
+            'portal_id' => $portal->id,
+        ]);
+        $client->assignRole('client');
     }
 }
